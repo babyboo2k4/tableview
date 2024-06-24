a9a4cfefad8ca3e362a44ebf09b4844c1ff617dc
babyboo2k4 <144683950+babyboo2k4@users.noreply.github.com>
2024-06-24 19:14:55 +0700

    Add files via upload

---
+++ HelloController.java
@@ -0,0 +1,14 @@
+package org.example.tbview;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
